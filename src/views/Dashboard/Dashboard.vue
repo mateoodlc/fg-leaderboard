2a6847f8803cfad971517488f74856be7d790e30
mateoodlc <mateoodlc@gmail.com>
2025-05-02 20:24:33 -0500

    feat: created views (Dashboard, Market)

---
+++ src/views/Dashboard/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header @refresh="gameStore.fetchMembers" />
+  <main class="main-content">
+    <GameMemberList />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import Header from '../../components/Header/Header.vue'
+import GameMemberList from '../../components/GameMemberList/GameMemberList.vue'
+import { useGameStore } from '../../stores/useGameStore'
+
+defineOptions({
+  name: 'dashboard',
+});
+const gameStore = useGameStore();
+
+</script>
---
+++ src/views/Market/Market.vue
@@ -0,0 +1,25 @@
+<template>
+  <Header @refresh="gameStore.fetchMarket" />
+  <main class="market__content">
+    <ul class="market__list">
+      <li v-memo class="market__list-item" v-for="item in gameStore.marketItems" :key="item.id">
+        <MarketCard :marketItem="item" />
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Header from '../../components/Header/Header.vue'
+import MarketCard from '../../components/MarketCard/MarketCard.vue'
+import { useGameStore } from '../../stores/useGameStore'
+
+const gameStore = useGameStore()
+defineEmits<{
+  (e: 'refresh'): void
+}>()
+</script>
+
+<style lang="scss" scoped>
+@use './market.scss' as *;
+</style>
