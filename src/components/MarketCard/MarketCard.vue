410a2064a729b42a7e3f3e8cdccc5c08b77b95a4
mateoodlc <mateoodlc@gmail.com>
2025-05-02 20:23:19 -0500

    feat: created market card component

---
+++ src/components/MarketCard/MarketCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="market-card">
+    <div class="market-card__icon-holder">
+      <MarketIcon :marketItem="marketItem.name" />
+    </div>
+    <div class="market-card__info">
+      <h2>{{ marketItem.name }}</h2>
+      <div class="market-card__info-price">
+        <Gold />
+        <p>{{ getFormattedNumber(marketItem.cost) }}</p>
+      </div>
+      <p class="market-card__description">{{ marketItem.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { MarketItemType } from '../../types';
+import Gold from '../../assets/icons/Gold.vue';
+import MarketIcon from '../../assets/icons/MarketIcon.vue';
+import getFormattedNumber from '../../utils/NumberFormatter'
+
+defineProps<{
+  marketItem: MarketItemType
+}>()
+</script>
+
+<style lang="scss" scoped>
+@use './marketCard.scss' as *;
+</style>
